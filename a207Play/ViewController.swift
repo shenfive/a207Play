f40298ec91c984eb56be690dfe73ab5c7b10b6a7
Danny Shen <shenfive@gmail.com>
2020-09-19 16:16:23 +0800

    Remote

--- a207Play/ViewController.swift
+++ a207Play/ViewController.swift
@@ -7,12 +7,14 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController,NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var theTableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        theTableView.delegate = self
+        theTableView.dataSource = self
+ 
     }
 
     override var representedObject: Any? {
@@ -21,6 +23,23 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func play(_ sender: NSButton) {
+        print("play")
+    }
+    
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = NSText()
+        cell.string = "Hello"
+        print(tableColumn)
+        return cell
+    }
+    
+    
+    
 }
 

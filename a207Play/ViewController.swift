43fab9e6090d49db4da90a03bc967a4b1983b208
Danny Shen <shenfive@gmail.com>
2020-09-19 15:46:15 +0800

    Initial Commit

---
+++ a207Play/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  a207Play
+//
+//  Created by 申潤五 on 2020/9/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
